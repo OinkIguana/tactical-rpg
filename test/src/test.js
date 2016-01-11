de5278178a12a18a42d7a39473d1fec7c4120cbb
Cameron Eldridge <cameron_e@rogers.com>
2016-01-11 09:06:23 -0500

    Added webpack

--- test/src/test.js
+++ test/src/test.js
@@ -2,5 +2,5 @@
 import {expect} from 'chai';
 
 describe('fire-emblem', () => {
-    it('should exist', () => expect(true).to.be.ok);
+    it('should exist', () => {});
 });
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: './public_html/script/src/index.js',
+    output: {
+        path: './public_html/',
+        filename: 'script/main.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass') },
+            { test: /\.js$/, loader: 'babel' },
+            { test: /test.*\.js$/, loader: 'mocha!babel'},
+            { test: /\.(otf|ttf)$/, loader: 'url' },
+            { test: /\.(svg|png|jpe?g)$/, loader: 'image-webpack' }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style/main.css')
+    ],
+    devtool: 'source-map',
+    watch: true
+};
